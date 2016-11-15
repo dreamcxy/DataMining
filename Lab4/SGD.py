787e4dfe745e151d379f90932678e3ec15b9ba8b
dreamcxy <dreamcxy@gmail.com>
2016-11-15 13:04:23 +0800

    mistake

--- Lab4/SGD.py
+++ Lab4/SGD.py
@@ -7,7 +7,7 @@ def load_data(file_name):
     data_matrix = []
     data_label = []
     with open(file_name, 'r') as dataFile:
-        data_lines = dataFile.readlines()
+        data_lines = dataFile.readlines()[:30000]
         for line in data_lines:
             data = map(float, line.split(','))
             data_matrix.append(data[:-1])
@@ -20,20 +20,20 @@ def calculate_norm_sub_gradient(theta, h=0.00001):
 
 
 def gradient_log_likelihood(data_matrix, data_label, lamda, theta, m):
-    i = random.randint(0, m - 1)
-    return (lamda * calculate_norm_sub_gradient(theta) / m) + ((-data_label[i] * sum(data_matrix[i])) * exp(
+    i = random.randint(0, m)
+    return (lamda * calculate_norm_sub_gradient(theta) / m) + (-data_label[i] * data_matrix[i] * exp(
         -data_label[i] * dot(theta, data_matrix[i].T))) / (m * (
             1 + exp(-data_matrix[i] * dot(theta, data_matrix[i].T))))
 
 
 def gradient_ridge_regression(data_matrix, data_label, lamda, theta, m):
-    i = random.randint(0, m - 1)
-    return (2 * sum(data_matrix[i]) * (dot(theta, data_matrix[i].T) - data_label[i]) + 2 * lamda * sum(
+    i = random.randint(0, m)
+    return (2 * data_matrix[i] * (dot(theta, data_matrix[i].T) - data_label[i]) + 2 * lamda * sum(
         data_matrix[i])) / m
 
 
 def upgrade_theta(theta, alpha, gradient_function, data_matrix, data_label, lamda, m):
-    return theta - alpha * gradient_function(data_matrix, data_label, lamda, theta, m)
+    return theta + alpha * gradient_function(data_matrix, data_label, lamda, theta, m)
 
 
 def log_likelihood(theta, lamda, data_matrix, data_label, m):
@@ -46,7 +46,7 @@ def ridge_regression(theta, lamda, data_matrix, data_label, m):
         linalg.norm(theta, 2), 2)
 
 
-def minimize_stochastic(file_name, target_function, gradient_function, iterations, lamda=1, alpha_0=0.1):
+def minimize_stochastic(file_name, target_function, gradient_function, iterations, lamda=0.01, alpha_0=0.01):
     data_matrix, data_label = load_data(file_name)
     m, n = data_matrix.shape
     theta_0 = zeros(n)
@@ -61,20 +61,20 @@ def minimize_stochastic(file_name, target_function, gradient_function, iteration
         #     alpha = alpha_0
         # else:
         #     iterations_num += 1
-        #     alpha *= 0.9
+        #     alpha *= 0.9C
         theta = upgrade_theta(theta, alpha, gradient_function,
                               data_matrix, data_label, lamda, m)
         iterations_num += 1
-        print theta
     return theta
 
 
 def calculate_probability(theta, x):
     # return exp(dot(theta, x.T)) / float(1 + exp(dot(theta, x.T)))
-    return 1 / (1 + exp(dot(theta, x.T)))
+    return exp(dot(theta, x.T)) / (1 + exp(dot(theta, x.T)))
 
 
-def calculate_error(data_matrix, data_label, theta, m):
+def calculate_error(data_matrix, data_label, theta):
+    m, n = data_matrix.shape
     count = 0
     calculate_label = []
     for i in range(0, m - 1):
@@ -84,18 +84,18 @@ def calculate_error(data_matrix, data_label, theta, m):
             calculate_label.append(-1)
         if calculate_label[i] == data_label[i]:
             count += 1
+    print count
     return count / float(m)
 
 
-file_name_train = 'training1.txt'
-file_name_test = 'testing1.txt'
+file_name_train = 'training2.txt'
+file_name_test = 'testing2.txt'
 data_matrix, data_label = load_data(file_name_train)
 m, n = data_matrix.shape
-# theta = minimize_stochastic(
-#     file_name, ridge_regression, gradient_ridge_regression, 100)
-theta = minimize_stochastic(
-    file_name, ridge_regression, gradient_ridge_regression, 1000)
 data_matrix_test, data_label_test = load_data(file_name_test)
 
-print calculate_error(data_matrix, data_label, theta, m)
-print calculate_error(data_matrix_test, data_label_test, theta, m)
+for i in range(1, 100):
+    theta = minimize_stochastic(
+        file_name_train, log_likelihood, gradient_log_likelihood, 10 * i)
+    print calculate_error(data_matrix, data_label, theta)
+    print calculate_error(data_matrix_test, data_label_test, theta)
